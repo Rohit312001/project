73c27576af467bb8bdb6855a95c6e2e6b6c0b93e
Rohit Rajput <76991475+Rohit312001@users.noreply.github.com>
2022-10-22 00:42:10 +0530

    recursion file added

---
+++ recursion.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+void func(int n){
+    if(n>0){
+    func(n-1);
+    printf("%d ",n);
+    }
+}
+int main(){
+    int x=3;
+    func(x);
+
+
+
+    return 0;
+}
